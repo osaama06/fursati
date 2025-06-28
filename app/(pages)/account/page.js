ef39bc76675417afaf8ad054c9ec0725a48c14dd
osaama06 <0rumoosh@gmail.com>
2025-06-28 03:38:05 +0300

    Update page.js
    correcting the path of logout button

--- app/(pages)/account/page.js
+++ app/(pages)/account/page.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import Image from 'next/image';
 import Link from 'next/link';
 import '@/styles/account.css';
-import LogoutButton from '@/app/components/logoutbutton/page';
+import LogoutButton from '../../components/logoutbutton/page';
 import { redirect } from 'next/navigation';
 
 const secret = process.env.JWT_SECRET;

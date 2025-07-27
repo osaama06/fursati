ee65e8e0a5fd3b75144eb461e49d95cc67062979
osaama06 <0rumoosh@gmail.com>
2025-07-28 01:09:42 +0300

    Update page.js

--- app/(pages)/account/page.js
+++ app/(pages)/account/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import '@/styles/account.css';
 // import LogoutButton from '@/app/components/logoutbutton/page';
-\import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 const secret = process.env.JWT_SECRET;
 
